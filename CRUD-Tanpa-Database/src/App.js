49b1b6c2b94b17584843f5292fdc0cb6d2fd45ea
Muhammad Fajar <mfjr48@gmail.com>
2020-08-06 00:22:20 +0700

    Upload file-file CRUD ReactJS (No DB)
    Menggunakan ReactJS untuk CRUD. Hanya manipulasi frontend (sementara), tidak pakai database.

---
+++ CRUD-Tanpa-Database/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./App.css";
+// import TabelBuah from "./tugas11/Tabel";
+// import Timer from "./tugas12/Timer";
+import TabelBuah from "./tugas13/Tabel";
+
+// App.js ini menggunakan CSS dari App.css
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* Tugas 1 */}
+        {/* <TabelBuah /> */}
+
+        {/* Tugas 2 */}
+        {/* <Timer /> */}
+
+        {/* Tugas 3 */}
+        <TabelBuah />
+      </div>
+    );
+  }
+}
+
+export default App;
