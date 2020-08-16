1b87e49c2c167e8b3df35d2ddeb2b9ec6f74ae63
Muhammad Fajar <mfjr48@gmail.com>
2020-08-16 21:09:01 +0700

    Contoh closure counter

--- Coba Closure/script.js
+++ Coba Closure/script.js
@@ -1,11 +1,10 @@
-function ucapkanSalam(waktu) {
-  return function (nama) {
-    console.log(`Halo ${nama}, Selamat ${waktu}. Semoga harimu menyenangkan!`);
+let add = (function () {
+  let counter = 0;
+  return function () {
+    return ++counter;
   };
-}
+})();
 
-let selamatPagi = ucapkanSalam("Pagi");
-let selamatSiang = ucapkanSalam("Siang");
-let selamatMalam = ucapkanSalam("Malam");
-
-console.dir(selamatMalam);
+console.log(add());
+console.log(add());
+console.log(add());
