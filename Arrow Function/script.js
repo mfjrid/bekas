d3a8dd6b68fbd9eaff5343b85b91948d9e8bd95f
Muhammad Fajar <mfjr48@gmail.com>
2020-08-18 15:15:05 +0700

    Konsep this pada Arrow Function

--- Arrow Function/script.js
+++ Arrow Function/script.js
@@ -1,9 +1,14 @@
-// Konsep this pada Constructor Function
+// Konsep this pada Arrow Function
 const Mahasiswa = function () {
-  (this.nama = "Fajar"), (this.umur = 23);
+  this.nama = "Fajar";
+  this.umur = 23;
   this.sayHello = function () {
     console.log(`Halo, nama saya ${this.nama} dan saya ${this.umur} tahun.`);
   };
+
+  setInterval(() => {
+    console.log(this.umur++);
+  }, 500);
 };
 
 const fajar = new Mahasiswa();
