873448bca5dcbf3560b63fb490870e424feff97c
Muhammad Fajar <mfjr48@gmail.com>
2020-08-18 14:52:26 +0700

    Arrow Function Mapping to Object

--- Arrow Function/script.js
+++ Arrow Function/script.js
@@ -1,4 +1,7 @@
-// Arrow Function Mapping
+// Arrow Function Mapping to Object
 let mahasiswa = ["Muhammad Fajar", "Irwan Ardiansyah", "Supriadi Manalu"];
-let jumlahHuruf = mahasiswa.map((nama) => nama.length);
+let jumlahHuruf = mahasiswa.map((nama) => ({
+  nama: nama,
+  jmlHuruf: nama.length,
+}));
 console.log(jumlahHuruf);
