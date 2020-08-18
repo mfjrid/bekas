67ec18f919482813a49705c4cf42ebe4ae53d2a7
Muhammad Fajar <mfjr48@gmail.com>
2020-08-18 15:30:06 +0700

    DOM dengan Arrow Function

--- Arrow Function/script.js
+++ Arrow Function/script.js
@@ -1,14 +1,7 @@
-// Konsep this pada Arrow Function
-const Mahasiswa = function () {
-  this.nama = "Fajar";
-  this.umur = 23;
-  this.sayHello = function () {
-    console.log(`Halo, nama saya ${this.nama} dan saya ${this.umur} tahun.`);
-  };
-
-  setInterval(() => {
-    console.log(this.umur++);
-  }, 500);
-};
-
-const fajar = new Mahasiswa();
+const box = document.querySelector(".box");
+box.addEventListener("click", function () {
+  this.classList.toggle("size");
+  setTimeout(() => {
+    this.classList.toggle("caption");
+  }, 600);
+});
