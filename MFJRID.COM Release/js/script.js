b9195a2aa6e984a53d884fc34d1e4fd773e7fa07
Muhammad Fajar <mfjr48@gmail.com>
2020-09-24 19:01:34 +0700

    Menambahkan icon dribbble

--- MFJRID.COM Release/js/script.js
+++ MFJRID.COM Release/js/script.js
@@ -1,1 +1,15 @@
 feather.replace();
+
+function munculText() {
+  var x = document.getElementById("hover-icon");
+  x.style.visibility = "visible";
+  x.style.opacity = "1";
+  x.style.transition = "0.5s";
+}
+
+function hilangText() {
+  var x = document.getElementById("hover-icon");
+  x.style.visibility = "hidden";
+  x.style.opacity = "0";
+  x.style.transition = "0.5s";
+}
