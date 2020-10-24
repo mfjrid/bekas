599915fe1af6b883c4df0a70cbaedaf8e8df4913
Muhammad Fajar <mfjr48@gmail.com>
2020-10-24 16:21:31 +0700

    Menambahkan modal

--- MFJRID.COM/portfolio/script.js
+++ MFJRID.COM/portfolio/script.js
@@ -8,18 +8,29 @@ $.ajax({
       if (el.tipe == "App Design" || el.tipe == "Web Design") {
         tombol = "Lihat Detail";
       }
-      cards += `<div class="col-md-3 my-4 kotak-card">
+      cards += `<div class="col-md-4 my-4 kotak-card">
                 <div class="card">
-                <img src="${el.image_url}" class="card-img-top" alt="${el.judul}" onmouseover="hoverImage()" />
+                <a href="#${el.unique}" data-toggle="modal">
+                <img src="${el.image_url}" class="card-img-top" alt="${el.judul}" />
+                </a>
                 <div class="card-body">
                     <h4 class="card-title">${el.judul}</h4>
                     <p class="card-text text-muted">
                     ${el.tanggal}
                     </p>
-                    <a href="${el.link_url}" class="tombol utama">${tombol}</a>
+                    <a href="#${el.unique}" data-toggle="modal" class="tombol utama">${tombol}</a>
                 </div>
                 </div>
-            </div>`;
+              </div>
+
+              <div class="modal fade" id="${el.unique}">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <h1>${el.judul}</h1>
+                </div>
+              </div>
+              </div>
+            `;
     });
     $(".portfolio-container").html(cards);
   },
@@ -27,11 +38,3 @@ $.ajax({
     console.log(e.responseText);
   },
 });
-
-function hoverImage() {
-  var imageIni = document.querySelectorAll(".card-img-top");
-  console.log(imageIni);
-  let myArray = [...imageIni];
-  console.log(myArray);
-  myArray.style.visibility = "hidden";
-}
