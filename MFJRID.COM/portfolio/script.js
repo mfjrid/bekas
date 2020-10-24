ec9238f5149179dfea81e0dc9cca403120a64c4f
Muhammad Fajar <mfjr48@gmail.com>
2020-10-24 15:30:50 +0700

    DOM Manipulation

--- MFJRID.COM/portfolio/script.js
+++ MFJRID.COM/portfolio/script.js
@@ -6,18 +6,17 @@ $.ajax({
     reversedResults.forEach((el) => {
       tombol = "Kunjungi Halaman";
       if (el.tipe == "App Design" || el.tipe == "Web Design") {
-        tombol = "Perbesar Gambar";
+        tombol = "Lihat Detail";
       }
       cards += `<div class="col-md-3 my-4 kotak-card">
                 <div class="card">
-                <img src="${el.image_url}" class="card-img-top" alt="${el.judul}" />
+                <img src="${el.image_url}" class="card-img-top" alt="${el.judul}" onmouseover="hoverImage()" />
                 <div class="card-body">
                     <h4 class="card-title">${el.judul}</h4>
                     <p class="card-text text-muted">
                     ${el.tanggal}
                     </p>
-                    <a href="#" class="tombol utama">Lihat Detail</a>
-                    <a href="${el.link_url}" class="tombol kedua">${tombol}</a>
+                    <a href="${el.link_url}" class="tombol utama">${tombol}</a>
                 </div>
                 </div>
             </div>`;
@@ -28,3 +27,11 @@ $.ajax({
     console.log(e.responseText);
   },
 });
+
+function hoverImage() {
+  var imageIni = document.querySelectorAll(".card-img-top");
+  console.log(imageIni);
+  let myArray = [...imageIni];
+  console.log(myArray);
+  myArray.style.visibility = "hidden";
+}
