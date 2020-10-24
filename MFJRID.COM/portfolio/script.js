53bd06cec80bdc08abdfd7ea7b7410f38f5341a4
Muhammad Fajar <mfjr48@gmail.com>
2020-10-24 17:58:02 +0700

    Fix Modal dan tombolnya

--- MFJRID.COM/portfolio/script.js
+++ MFJRID.COM/portfolio/script.js
@@ -6,7 +6,7 @@ $.ajax({
     reversedResults.forEach((el) => {
       tombol = "Kunjungi Halaman";
       if (el.tipe == "App Design" || el.tipe == "Web Design") {
-        tombol = "Lihat Detail";
+        tombol = "Perbesar Gambar";
       }
       cards += `<div class="col-md-4 my-4 kotak-card">
                 <div class="card">
@@ -18,15 +18,28 @@ $.ajax({
                     <p class="card-text text-muted">
                     ${el.tanggal}
                     </p>
+                    <a href="#${el.unique}" data-toggle="modal" class="tombol kedua">Lihat Detail</a>
                     <a href="#${el.unique}" data-toggle="modal" class="tombol utama">${tombol}</a>
                 </div>
                 </div>
               </div>
 
               <div class="modal fade" id="${el.unique}">
-              <div class="modal-dialog modal-lg">
+              <div class="modal-dialog modal-xl">
                 <div class="modal-content">
-                  <h1>${el.judul}</h1>
+                <div class="modal-header">
+                  <h5 class="modal-title font-weight-bold">${el.judul}</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                  <div class="container p-3">
+                    <div class="row">
+                      <div class="col-md-12">
+                      <img src="${el.image_url}" alt="${el.judul}" />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
               </div>
